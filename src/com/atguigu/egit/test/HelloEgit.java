d6933c0274ac96050b8a159ce37a0407103bc309
Fly599999 <907138720@qq.com>
2019-11-05 15:20:52 +0800

    创建新文件

---
+++ src/com/atguigu/egit/test/HelloEgit.java
@@ -0,0 +1,11 @@
+package com.atguigu.egit.test;
+
+public class HelloEgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloEgit");
+		System.out.println("¸üÐÂ1");
+	}
+
+}
